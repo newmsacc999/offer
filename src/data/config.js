cc3268318436e0fd027013099343d43e8b40f3ff
newmsacc999 <newmsacc999@hotmail.com>
2026-02-17 12:10:21 +0530

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -19,7 +19,7 @@ export const INITIAL_SETTINGS = {
   pay_type: false, // false = UPI (pay_type_1), true = Common (pay_type_2)
   payment_script: "",
   allowed_ip: "",
-  upi: "fsv.470000099389679@icici",
+  upi: "fsv.470000099385044@icici",
   pixel: "",
   is_maintenance: false, // Added to support client-side maintenance mode
 };
